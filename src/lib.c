946b3d5e6943f05ba5ef7a3189d7223975ec2fed
ElrohirGT <elrohirgt@gmail.com>
2025-03-04 11:30:45 -0600

    feat: Added some basic lib structures

--- src/lib.c
+++ src/lib.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
+#include <stdint.h>
 
-// Hello
-int main(int argc, char **argv) {
-  printf("Hello world!\n");
-  return 0;
-}
+// Represents a string "slice"
+//
+// Only the creator of the original slice needs to free this memory!
+struct UWU_String {
+  // Contains all the data of this string
+  // CAREFUL: It may or may not be a "null terminated string"!
+  uint8_t *data;
+  // The length of data that is considered to be "this string"
+  uint32_t length;
+};
+
+// Represents a message on a given chat history.
+//
+// The ChatEntry should own it's memory! So it should receive a copy of
+// `content` and `origin_username`.
+struct UWU_ChatEntry {
+  // The content of the message.
+  struct UWU_String content;
+  // The username of the person sending the message.
+  struct UWU_String origin_username;
+};
+
+// Represents a message history of a certain chat
+struct UWU_History {
+  // A pointer to an array of `ChatEntrie`.
+  struct UWU_ChatEntry *messages;
+  // The number of chat messages filling the array.
+  uint32_t count;
+  // How much memory is left in the array of `ChatEntrie`.
+  uint32_t capacity;
+};
